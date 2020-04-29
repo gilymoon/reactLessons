eca421d3606f9c51cfa078f021d516331c80b173
unknown <gilyjmoon@gmail.com>
2020-04-29 15:02:41 +0300

    1

--- lesson3/task1/src/index.jsx
+++ lesson3/task1/src/index.jsx
@@ -2,21 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.scss';
 import './search.scss';
+import Search from './Search.jsx';
 
 const rootElement = document.querySelector('#root');
 
-const SearchFiled = (props) => {
-  const element = (
-    <div className="search">
-      <h1 className="search__title">{`Hello, ${props.name}. What to search for you?`}</h1>
-      <div className="search__field">
-        <input type="text" className="search__input"/>
-        <button className="search__button">Search</button>
-      </div>
-    </div>
-  )
-    return element;
-}
 
 
-ReactDOM.render(<SearchFiled name="Bob"/>, rootElement);
+
+ReactDOM.render(<Search name="Bob"/>, rootElement);
