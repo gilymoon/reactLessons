f7a32c8892299935fa828a6a92066eeb37f67971
unknown <gilyjmoon@gmail.com>
2020-04-29 15:06:53 +0300

    2

--- lesson3/task2/src/index.jsx
+++ lesson3/task2/src/index.jsx
@@ -1,7 +1,8 @@
-import React, {Component} from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.scss';
 import './search.scss';
+import Search from './Search.jsx'
 
 const rootElement = document.querySelector('#root');
 
@@ -18,22 +19,22 @@ const rootElement = document.querySelector('#root');
 //     return element;
 // }
 
-class SearchField extends Component{
-  render() {
+// class SearchField extends Component{
+//   render() {
     
-      return (
-            <div className="search">
-              <h1 className="search__title">{`Hello, ${this.props.name}. What to search for you?`}</h1>
-              <div className="search__field">
-                <input type="text" className="search__input"/>
-                <button className="search__button">Search</button>
-              </div>
-            </div>
-          )
+//       return (
+//             <div className="search">
+//               <h1 className="search__title">{`Hello, ${this.props.name}. What to search for you?`}</h1>
+//               <div className="search__field">
+//                 <input type="text" className="search__input"/>
+//                 <button className="search__button">Search</button>
+//               </div>
+//             </div>
+//           )
             
        
       
-      }
-}
+//       }
+// }
 
-ReactDOM.render(<SearchField name="Bob"/>, rootElement);
+ReactDOM.render(<Search name="Bob"/>, rootElement);
