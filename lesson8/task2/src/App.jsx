b49a7e6474e0ce2b2032cde8309463ebf4e4354d
unknown <gilyjmoon@gmail.com>
2020-05-06 14:16:21 +0300

    2

--- lesson8/task2/src/App.jsx
+++ lesson8/task2/src/App.jsx
@@ -2,28 +2,32 @@ import React, { Component } from "react";
 import Clock from "./Clock";
 
 class App extends Component {
-  constructor(props){
+  constructor(props) {
     super(props);
     this.state = {
-      visible: true
-    }
-
+      visible: true,
+    };
     this.toggle = this.toggle.bind(this);
   }
-  
+
   toggle() {
     this.setState({
-      visible: !this.state.visible
-    })
+      visible: !this.state.visible,
+    });
   }
   render() {
     return (
-
       <div>
-      <button onClick={this.toggle}>Toggle</button>
-      <div>{this.state.visible && <Clock />}</div>
-    </div>
-  )
+        <button onClick={this.toggle}>Toggle</button>
+        {this.state.visible && (
+          <>
+            <Clock location={"New York"} offset={-5} />
+            <Clock location={"Kyiv"} offset={3} />
+            <Clock location={"London"} offset={0} />
+          </>
+        )}
+      </div>
+    );
   }
 }
 
--- lesson8/task2/src/Clock.jsx
+++ lesson8/task2/src/Clock.jsx
@@ -1,32 +1,37 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import moment from 'moment';
+import './clock.scss';
 
-class Clock extends Component{
-  constructor(props){
-    super(props);
-this.state = {
-  date: new Date()
-};
+const formatDate = offset => moment().utcOffset(offset).format('LTS');
 
+class Clock extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            location: props.location,
+            offset: formatDate(props.offset),
+        };
+    }
 
-  }
+    componentDidMount() {
+        this.interval = setInterval(() => {
+            this.setState({
+                offset: formatDate(this.state.offset),
+            });
+        }, 1000);
+    }
 
-  componentDidMount(){
+    componentWillUnmount() {
+        clearInterval(this.interval)
+    }
 
-    this.interval = setInterval(() => {
-      this.setState({
-        date: new Date()
-      })
-    }, 1000);
-    
-  }
-
-  componentWillUnmount(){
-    clearInterval(this.interval);
-  }
-
-  render(){
-  return <div>{this.state.date.toLocaleTimeString()}</div>;
-  }
+    render() {
+        return (
+        <div className="clock">
+                <div className="clock__location">{this.state.location}</div>
+                <div className="clock__time">{this.state.offset}</div>
+        </div>);
+    }
 }
 
 export default Clock;
